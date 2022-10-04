fcafacfe97052c73cb2f0c1c34942c5e57766fb2
Elkhan Julian Brillianshah <ubbeg2000@gmail.com>
2022-10-04 21:51:12 +0700

    change: revisi on_off_fsm biar kalo dari off ke on dan diteken kelamaan gak tbtb mati

--- on-off/fsm/on_off_fsm.c
+++ on-off/fsm/on_off_fsm.c
@@ -8,24 +8,31 @@ void on_off_fsm(int *state, int *cnt, int input, int *output)
     {
         if (input == 1)
         {
-            if (*cnt >= OFF_DELAY)
-            {
-                *cnt = *cnt + 1;
-                *state = OFF_STATE;
-                *output = 0;
-            }
-            else
-            {
-                *cnt = 0;
-                *state = ON_STATE;
-                *output = 1;
-            }
+            *cnt = 0;
+            *state = JUST_ON_STATE;
+            *output = 1;
         }
         else
         {
             *cnt = 0;
             *state = OFF_STATE;
             *output = 0;
+        }
+        break;
+    }
+    case JUST_ON_STATE:
+    {
+        if (input == 1)
+        {
+            *cnt = 0;
+            *output = 1;
+            *state = JUST_ON_STATE;
+        }
+        else
+        {
+            *cnt = 0;
+            *output = 1;
+            *state = ON_STATE;
         }
         break;
     }
@@ -41,8 +48,8 @@ void on_off_fsm(int *state, int *cnt, int input, int *output)
             }
             else
             {
-                *cnt = *cnt + 1;
-                *state = OFF_STATE;
+                *cnt = 0;
+                *state = JUST_OFF_STATE;
                 *output = 0;
             }
         }
@@ -51,6 +58,22 @@ void on_off_fsm(int *state, int *cnt, int input, int *output)
             *cnt = 0;
             *state = ON_STATE;
             *output = 1;
+        }
+        break;
+    }
+    case JUST_OFF_STATE:
+    {
+        if (input == 1)
+        {
+            *cnt = 0;
+            *output = 0;
+            *state = JUST_OFF_STATE;
+        }
+        else
+        {
+            *cnt = 0;
+            *output = 0;
+            *state = OFF_STATE;
         }
         break;
     }
--- on-off/fsm/on_off_fsm.h
+++ on-off/fsm/on_off_fsm.h
@@ -1,5 +1,7 @@
 #define OFF_STATE 80
-#define ON_STATE 81
-#define OFF_DELAY 500
+#define JUST_ON_STATE 81
+#define ON_STATE 82
+#define JUST_OFF_STATE 83
+#define OFF_DELAY 200
 
 void on_off_fsm(int *state, int *cnt, int input, int *output);
